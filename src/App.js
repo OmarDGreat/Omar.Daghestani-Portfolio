e893af786a6f8d03ba49c2efb4cbaa8f8a46ba63
OmarD <omar.daghestani@colorado.edu>
2022-11-12 15:49:29 -0700

    work in progress

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Nav from "./components/Nav";
+// import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+// import ContactForm from "./components/Contact";
+// import Footer from "./components/Footer";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <Portfolio></Portfolio>
+
+    </div>
+  );
+}
+
+export default App;
