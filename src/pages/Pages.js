0ab567bb24818cfe077c89a8d0ceaabb105d0dd8
OmarD <omar.daghestani@colorado.edu>
2022-11-16 15:37:26 -0700

    done?

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Home from './Home'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
-import Resume from '../components/Resume'
 import { Route, Routes, BrowserRouter, HashRouter, Link } from 'react-router-dom';
 
 
@@ -14,7 +13,6 @@ function Pages() {
       <Route path="/Omar.Daghestani-Portfolio" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} /> 
       <Route path="/contact" element={<Contact />} />
-      <Route path="/resume" element={<Resume />} />
     </Routes>
     </BrowserRouter>
   )
