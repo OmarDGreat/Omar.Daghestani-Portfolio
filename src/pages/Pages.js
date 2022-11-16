ee4e8e4f7af6fa93c34ccb5fd533beffc4c10e69
OmarD <omar.daghestani@colorado.edu>
2022-11-16 16:43:24 -0700

    Works

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Home from './Home'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
-import { Route, Routes, BrowserRouter, HashRouter, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 
 function Pages() {
   return (
     
-    <BrowserRouter>
     <Routes>
       <Route path="/Omar.Daghestani-Portfolio" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} /> 
       <Route path="/contact" element={<Contact />} />
     </Routes>
-    </BrowserRouter>
+    
   )
 }
 
